e36cdfbc08b74354d02ce35501d21a48096c1a1b
송민정 <mjm6k.naver.com>
2024-08-01 14:42:14 +0900

    Feat : videoeditorSDK 라이브러리 설치

--- src/screens/SlicingScreen.js
+++ src/screens/SlicingScreen.js
@@ -1,48 +1,64 @@
-import Video from "react-native-video";
-import React from "react";
+import React, { useEffect } from "react";
 import {
   View,
-  StyleSheet
-} from 'react-native'
-import LargeButton from '../components/LargeButton'
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  NativeEventEmitter,
+  NativeModules,
+  Button,
+} from "react-native";
+import {
+  VESDK,
+  VideoEditorModal,
+  Configuration,
+} from "react-native-videoeditorsdk";
 
-const SlicingScreen = ({navigation}) => {
+// import Video from "react-native-video";
+import LargeButton from "../components/LargeButton";
 
+// VESDK.unlockWithLicense(require("./vesdk_license"))
+const SlicingScreen = ({ navigation }) => {
+  
 
   return (
     <View style={styles.container}>
-      <Video
+      <Button
+        title="버튼을 누르고 편집하기"
+        onPress={() => {
+          // VESDK.openEditor(require("../../assets/jwtest.mp4"))
+        }}
+      />
+      {/* <Video
         source={require("../../assets/jwtest.mp4")} // require를 사용하여 정적 자산을 로드
-        style={styles.fullScreen}
+        style={styles.video}
         paused={false} // 재생/중지 여부
-        resizeMode={"cover"} // 프레임이 비디오 크기와 일치하지 않을 때 비디오 크기를 조정하는 방법을 결정합니다. cover : 비디오의 크기를 유지하면서 최대한 맞게
-        onLoad={e => console.log(e)} // 미디어가 로드되고 재생할 준비가 되면 호출되는 콜백 함수입니다.
+        resizeMode={"cover"} // 프레임이 비디오 크기와 일치하지 않을 때 비디오 크기를 조정하는 방법을 결정합니다.
+        onLoad={(e) => console.log(e)} // 미디어가 로드되고 재생할 준비가 되면 호출되는 콜백 함수입니다.
         repeat={true} // video가 끝나면 다시 재생할 지 여부
         onAnimatedValueUpdate={() => {}}
-      />
+      /> */}
       <LargeButton
-        title='결과확인하러 가기'
-        toward='Loading'
+        title="결과확인하러 가기"
+        toward="Loading"
         navigation={navigation}
       />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f2f2f2',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f2f2f2",
   },
-  fullScreen: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
+  video: {
+    width: "100%",
+    height: 200,
+    backgroundColor: "#000",
   },
-})
+});
 
 export default SlicingScreen;
